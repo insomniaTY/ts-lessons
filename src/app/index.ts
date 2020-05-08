89307fad298afdfe413c26c3803111fbf23176ad
plutta <existenz.ty@gmail.com>
2020-05-08 14:54:32 +0300

    Typescript Lessons

---
+++ src/app/index.ts
@@ -0,0 +1,20 @@
+const b = document.querySelector('.row')?.children || [];
+
+// if(b !== undefined) {
+//   const b = document.querySelector('.row')?.children
+// };
+
+const a = document.images;
+console.log(a);
+
+// console.log(b);
+
+for(let i = 0; i < b.length; i++) {
+  console.log(i);
+}
+
+// let l;
+
+// l = document.createElement('h2')
+// l = document.compareDocumentPosition
+// console.log(l);
